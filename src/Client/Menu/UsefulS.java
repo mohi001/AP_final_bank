78165a873ad196b0e26c4d3c518a02fb3535d35f
mohammad hashemy <smhd.hashemy@outlook.com>
2021-07-04 22:03:56 +0430

    pay bills and useful (still not complet)

--- src/Client/Menu/UsefulS.java
+++ src/Client/Menu/UsefulS.java
@@ -8,24 +8,21 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class PayBillS extends Scene {
+public class UsefulS extends Scene {
     private static final GridPane root = new GridPane();
     String message = "";
     String number;
     String name;
     private Messenger ms;
 
-    //    public PayBills(Stage stage, Messenger messenger) throws FileNotFoundException {
-    public PayBillS(Stage stage) throws FileNotFoundException {
+    //    public UsefulS(Stage stage, Messenger messenger) throws FileNotFoundException {
+    public UsefulS(Stage stage) throws FileNotFoundException {
         super(root, 900, 700);
 //        ms = messenger;
 
@@ -52,35 +49,18 @@ public class PayBillS extends Scene {
                 }, "pleas inter only numbers", 0, 0, false
         );
 
-        InUpField serial = new InUpField("src/Client/Resources/sign_in.png", "pleas inter duration in month",
+        InUpField name = new InUpField("src/Client/Resources/sign_in.png", "pleas inter duration in month",
                 new ValidAble() {
                     @Override
                     public boolean isValid(String s) {
-                        try
+                        if (s.split("\\s").length == 1)
                         {
-                            Integer.parseInt(s);
                             return true;
-                        } catch (Exception e)
-                        {
-                            return false;
                         }
+                        return false;
                     }
-                }, "pleas only inter number", 0, 0, false
-        );
-        InUpField account = new InUpField("src/Client/Resources/sign_in.png", "pleas inter account",
-                new ValidAble() {
-                    @Override
-                    public boolean isValid(String s) {
-                        try
-                        {
-                            Double.parseDouble(s);
-                            return true;
-                        } catch (Exception e)
-                        {
-                            return false;
-                        }
-                    }
-                }, "pleas only inter number", 0, 0, false
+                }, "don't inter space", 0, 0, false
+
         );
 
         Label answer = new Label("pleas fill the fields\n" +
@@ -88,20 +68,15 @@ public class PayBillS extends Scene {
 
         Button req = new Button("request loan");
         req.setTranslateX(100);
-        MenuButton menuButton = getaccountM(ms, account);
-        menuButton.setTranslateX(50);
         answer.setTranslateY(-70);
-        root.add(serial, 0, 0);
         root.add(code, 0, 1);
-        root.add(account, 0, 2);
-        root.add(menuButton, 0, 3);
+        root.add(name, 0, 2);
         root.add(req, 0, 4);
         root.add(answer, 1, 2);
 
         ArrayList<InUpField> fields = new ArrayList<>();
-        fields.add(serial);
         fields.add(code);
-        fields.add(account);
+        fields.add(name);
 
         req.setOnAction(e -> {
             message = "bill" + " ";
@@ -119,22 +94,4 @@ public class PayBillS extends Scene {
         });
     }
 
-    private MenuButton getaccountM(Messenger ms, InUpField tf)
-    {
-        MenuButton m = new MenuButton("account aliases");
-        String answer = ms.send("MAliasL");
-        Scanner sc = new Scanner(answer);
-        while (sc.hasNextLine())
-        {
-            number = sc.next();
-            name = sc.next();
-            MenuItem menuItem = new MenuItem(name);
-            menuItem.setOnAction(e -> {
-                tf.setText(number);
-                m.setText(name);
-            });
-            m.getItems().add(menuItem);
-        }
-        return m;
-    }
 }
